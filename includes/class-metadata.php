6c44cb766057c732446e25e1f4ffd5ce3eec388e
Simon Dowdles <simon@simondowdles.com>
2017-10-26 14:39:25 +0200

    API change for updating post meta

--- includes/class-metadata.php
+++ includes/class-metadata.php
@@ -81,90 +81,38 @@ class Metadata {
 	 */
 	public function register_metabox() {
 		foreach ( $this->config as $section_id => $section ) {
-			$section = $this->parse_section( $section );
+            $section = $this->parse_section($section);
 
-			global $wp_meta_boxes;
+            global $wp_meta_boxes;
 
-			$id = "{$this->prefix}_{$section_id}";
-			$title = $section['label'];
-			$callback = array( $this, 'render_metabox' );
-			$screen = Post_Type::NAME;
-			$context = $section['context'];
-			$priority = $section['priority'];
-			$callback_args = array(
-				'section_id' => $section_id,
-			);
+            $id = "{$this->prefix}_{$section_id}";
+            $title = $section['label'];
+            $callback = array($this, 'render_metabox');
+            $screen = Post_Type::NAME;
+            $context = $section['context'];
+            $priority = $section['priority'];
+            $callback_args = array(
+                'section_id' => $section_id,
+            );
 
-			if ( empty( $screen ) ) {
-				$screen = get_current_screen();
-			} elseif ( is_string( $screen ) ) {
-				$screen = convert_to_screen( $screen );
-			} elseif ( is_array( $screen ) ) {
-				foreach ( $screen as $single_screen ) {
-					add_meta_box( $id, $title, $callback, $single_screen, $context, $priority, $callback_args );
-				}
-			}
+            if (empty($screen)) {
+                $screen = get_current_screen();
+            } elseif (is_string($screen)) {
+                $screen = convert_to_screen($screen);
+            } elseif (is_array($screen)) {
+                foreach ($screen as $single_screen) {
+                    add_meta_box($id, $title, $callback, $single_screen, $context, $priority, $callback_args);
+                }
+            }
 
-			if ( ! isset( $screen->id ) ) {
-				return;
-			}
+            if (!isset($screen->id)) {
+                return;
+            }
 
-			$page = $screen->id;
+            add_meta_box($id, $title, $callback, $screen, $context, $priority, $callback_args);
 
-			if ( !isset($wp_meta_boxes) )
-				$wp_meta_boxes = array();
-			if ( !isset($wp_meta_boxes[$page]) )
-				$wp_meta_boxes[$page] = array();
-			if ( !isset($wp_meta_boxes[$page][$context]) )
-				$wp_meta_boxes[$page][$context] = array();
+        }
 
-			foreach ( array_keys($wp_meta_boxes[$page]) as $a_context ) {
-				foreach ( array('high', 'core', 'default', 'low') as $a_priority ) {
-					if ( !isset($wp_meta_boxes[$page][$a_context][$a_priority][$id]) )
-						continue;
-
-					// If a core box was previously added or removed by a plugin, don't add.
-					if ( 'core' == $priority ) {
-						// If core box previously deleted, don't add
-						if ( false === $wp_meta_boxes[$page][$a_context][$a_priority][$id] )
-							return;
-
-						/*
-						 * If box was added with default priority, give it core priority to
-						 * maintain sort order.
-						 */
-						if ( 'default' == $a_priority ) {
-							$wp_meta_boxes[$page][$a_context]['core'][$id] = $wp_meta_boxes[$page][$a_context]['default'][$id];
-							unset($wp_meta_boxes[$page][$a_context]['default'][$id]);
-						}
-						return;
-					}
-					// If no priority given and id already present, use existing priority.
-					if ( empty($priority) ) {
-						$priority = $a_priority;
-					/*
-					 * Else, if we're adding to the sorted priority, we don't know the title
-					 * or callback. Grab them from the previously added context/priority.
-					 */
-					} elseif ( 'sorted' == $priority ) {
-						$title = $wp_meta_boxes[$page][$a_context][$a_priority][$id]['title'];
-						$callback = $wp_meta_boxes[$page][$a_context][$a_priority][$id]['callback'];
-						$callback_args = $wp_meta_boxes[$page][$a_context][$a_priority][$id]['args'];
-					}
-					// An id can be in only one priority and one context.
-					if ( $priority != $a_priority || $context != $a_context )
-						unset($wp_meta_boxes[$page][$a_context][$a_priority][$id]);
-				}
-			}
-
-			if ( empty($priority) )
-				$priority = 'low';
-
-			if ( !isset($wp_meta_boxes[$page][$context][$priority]) )
-				$wp_meta_boxes[$page][$context][$priority] = array();
-
-			$wp_meta_boxes[$page][$context][$priority][$id] = array('id' => $id, 'title' => $title, 'callback' => $callback, 'args' => $callback_args);
-		}
 	}
 
 	/**
@@ -310,112 +258,8 @@ class Metadata {
 							return false;
 						}
 
-						$column = sanitize_key($meta_type . '_id');
-						$id_column = 'user' == $meta_type ? 'umeta_id' : 'meta_id';
+                        update_post_meta( $post_id, $id, $value, '' );
 
-						// expected_slashed ($meta_key)
-						$raw_meta_key = $meta_key;
-						$meta_key = wp_unslash($meta_key);
-						$passed_value = $meta_value;
-						$meta_value = wp_unslash($meta_value);
-						$meta_value = sanitize_meta( $meta_key, $meta_value, $meta_type );
-
-						// Compare existing value to new value if no prev value given and the key exists only once.
-						if ( empty($prev_value) ) {
-							$old_value = get_metadata($meta_type, $object_id, $meta_key);
-							if ( count($old_value) == 1 ) {
-								if ( $old_value[0] === $meta_value )
-									return false;
-							}
-						}
-
-						$meta_ids = $wpdb->get_col( $wpdb->prepare( "SELECT $id_column FROM $table WHERE meta_key = %s AND $column = %d", $meta_key, $object_id ) );
-						if ( empty( $meta_ids ) ) {
-							return add_metadata( $meta_type, $object_id, $raw_meta_key, $passed_value );
-						}
-
-						$_meta_value = $meta_value;
-						$meta_value = maybe_serialize( $meta_value );
-
-						$data  = compact( 'meta_value' );
-						$where = array( $column => $object_id, 'meta_key' => $meta_key );
-
-						if ( !empty( $prev_value ) ) {
-							$prev_value = maybe_serialize($prev_value);
-							$where['meta_value'] = $prev_value;
-						}
-
-						$result = $wpdb->update( $table, $data, $where );
-						if ( ! $result )
-							return false;
-
-						wp_cache_delete($object_id, $meta_type . '_meta');
-					} else {
-						global $wpdb;
-
-						$meta_type = 'post';
-						$object_id = $post_id;
-						$meta_key = $id;
-						$meta_value = $value;
-						$delete_all = false;
-
-						if ( ! $meta_type || ! $meta_key || ! is_numeric( $object_id ) && ! $delete_all ) {
-							return false;
-						}
-
-						$object_id = absint( $object_id );
-						if ( ! $object_id && ! $delete_all ) {
-							return false;
-						}
-
-						$table = _get_meta_table( $meta_type );
-						if ( ! $table ) {
-							return false;
-						}
-
-						$type_column = sanitize_key($meta_type . '_id');
-						$id_column = 'user' == $meta_type ? 'umeta_id' : 'meta_id';
-						// expected_slashed ($meta_key)
-						$meta_key = wp_unslash($meta_key);
-						$meta_value = wp_unslash($meta_value);
-
-						$_meta_value = $meta_value;
-						$meta_value = maybe_serialize( $meta_value );
-
-						$query = $wpdb->prepare( "SELECT $id_column FROM $table WHERE meta_key = %s", $meta_key );
-
-						if ( !$delete_all )
-							$query .= $wpdb->prepare(" AND $type_column = %d", $object_id );
-
-						if ( '' !== $meta_value && null !== $meta_value && false !== $meta_value )
-							$query .= $wpdb->prepare(" AND meta_value = %s", $meta_value );
-
-						$meta_ids = $wpdb->get_col( $query );
-						if ( !count( $meta_ids ) )
-							return false;
-
-						if ( $delete_all ) {
-							$value_clause = '';
-							if ( '' !== $meta_value && null !== $meta_value && false !== $meta_value ) {
-								$value_clause = $wpdb->prepare( " AND meta_value = %s", $meta_value );
-							}
-
-							$object_ids = $wpdb->get_col( $wpdb->prepare( "SELECT $type_column FROM $table WHERE meta_key = %s $value_clause", $meta_key ) );
-						}
-
-						$query = "DELETE FROM $table WHERE $id_column IN( " . implode( ',', $meta_ids ) . " )";
-						$count = $wpdb->query($query);
-
-						if ( !$count )
-							return false;
-
-						if ( $delete_all ) {
-							foreach ( (array) $object_ids as $o_id ) {
-								wp_cache_delete($o_id, $meta_type . '_meta');
-							}
-						} else {
-							wp_cache_delete($object_id, $meta_type . '_meta');
-						}
 					}
 				}
 			}
